7c39b3e0141b78fd1337bfc903f46aaf3feac13a
hadley <h.wickham@gmail.com>
2008-08-19 10:36:11 -0500

    Make it possible to set number of columns

--- R/block-figure.r
+++ R/block-figure.r
@@ -1,10 +1,12 @@
-figure <- function(code, ..., envir = globalenv()) {
+figure <- function(code, ..., col = 2, envir = globalenv()) {
   woven <- weave(code, envir)  
   
+  i <- 0
   weave_figure <- weave_nul
   weave_figure$value <- function(x, ...) {
+    i <<- i + 1
     if (!inherits(x, "ggplot")) return()
-    save_plot_tex(x, comment = TRUE, ...)
+    save_plot_tex(x, comment = (i %% col != 0), ...)
   }
   weave_figure$start <- start_figure
   weave_figure$stop <- end_figure
